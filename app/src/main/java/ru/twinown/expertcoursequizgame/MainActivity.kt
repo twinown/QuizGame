f51090aa1756d51bfbdeff9166680073e1ffe4a3
twinown <niyaz9797@gmail.com>
2025-01-26 21:46:01 +0300

    QG-04 MainActivity

--- app/src/main/java/ru/twinown/expertcoursequizgame/MainActivity.kt
+++ app/src/main/java/ru/twinown/expertcoursequizgame/MainActivity.kt
@@ -17,8 +17,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //тут происходитпарсинг и созданиеActivityMainBinding
+        //тут происходит парсинг и созданиеActivityMainBinding
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val viewModel:GameViewModel = GameViewModel()
+
+        binding.firstChoiceButton.setOnClickListener {
+            val uiState :GameUiState = viewModel.chooseFirst()
+            //изменение состояния экрана
+            uiState.update(binding = binding)
+        }
+        binding.secondChoiceButton.setOnClickListener {
+            val uiState :GameUiState = viewModel.chooseSecond()
+            uiState.update(binding = binding)
+        }
+        binding.thirdChoiceButton.setOnClickListener {
+            val uiState :GameUiState = viewModel.chooseThird()
+            uiState.update(binding = binding)
+        }
+        binding.fourthChoiceButton.setOnClickListener {
+            val uiState :GameUiState = viewModel.chooseFourth()
+            uiState.update(binding = binding)
+        }
+        binding.checkButton.setOnClickListener {
+            val uiState :GameUiState = viewModel.chooseCheck()
+            uiState.update(binding = binding)
+        }
+        binding.nextButton.setOnClickListener {
+            val uiState :GameUiState = viewModel.chooseNext()
+            uiState.update(binding = binding)
+        }
+
+        //первое открытие приложения
+        val uiState:GameUiState = viewModel.init()
+        uiState.update(binding = binding)
+
+
     }
 }
