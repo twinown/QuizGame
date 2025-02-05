5bd539b4a9be4829042e612e4b76b2a7906c76e9
twinown <niyaz9797@gmail.com>
2025-02-05 19:40:40 +0300

    QG-06 UiStates and GameRepository.Base

--- app/src/main/java/ru/twinown/expertcoursequizgame/GameUiState.kt
+++ app/src/main/java/ru/twinown/expertcoursequizgame/GameUiState.kt
@@ -1,47 +1,68 @@
 package ru.twinown.expertcoursequizgame
 
+import android.view.View
 import ru.twinown.expertcoursequizgame.databinding.ActivityMainBinding
 
 interface GameUiState {
-    fun update(binding: ActivityMainBinding) :Unit= throw IllegalStateException("")
+    fun update(binding: ActivityMainBinding)
+
+    //свойства из блюпринта!!!
+    //тк у нас для каждого экрана имеются состояния всех семи элементов
+    //то мы иъ пишем все в одном абстрактном классе, а не в каждом наследнике =
+    //не нарушаем DRY
+    abstract class Abstract(
+        private val questionText: String,
+        private val choicesStateList: List<ChoiceUiState>,
+        private val checkVisibility: Int,
+        private val nextVisibility: Int
+    ) : GameUiState {
+        override fun update(binding: ActivityMainBinding) = with((binding)) {
+            questionTextView.text = questionText
+            choicesStateList[0].update(firstChoiceButton)
+            choicesStateList[1].update(secondChoiceButton)
+            choicesStateList[2].update(thirdChoiceButton)
+            choicesStateList[3].update(fourthChoiceButton)
+            checkButton.visibility = checkVisibility
+            nextButton.visibility = nextVisibility
+        }
+    }
 
     //TODO ПОЧЕМУ ДАТА КЛАССЫ ?????????????????????ответ ниже
     //потому что у нас будет проверяться текст, а потлму в дассаъ должны быть переопределены
     //иквалс  и хэшкод-> дата класс,поэтому
-  data  class AskedQuestion(val question: String,
-                           val choices: List<String>) :
-        GameUiState {
+    //это начальное состояние экрана типо
+    data class AskedQuestion(
+        private val question: String,
+        private val choices: List<String>
+    ) : Abstract(
+        question,
+        choices.map { ChoiceUiState.AvailableToChoose(it) },
+        checkVisibility = View.INVISIBLE,
+        nextVisibility = View.INVISIBLE
+    )
 
-    }
+    //todo nпочему здесь не сделали выбранному NotAvailableToChoose
+    //сделали, но во вьюмодельке
+    data class ChoiceMade(
+        private val question: String,
+        private val choices: List<ChoiceUiState>
+    ) : Abstract(
+        question,
+        //почему тут уже по-другому ?
+        choices,
+        checkVisibility = View.VISIBLE,
+        nextVisibility = View.INVISIBLE
+    )
 
-  data  class ChoiceMade(val question: String,
-                         val choices: List<ChoiceUiState>) :
-        GameUiState {
-
-    }
-
-   data class AnswerChecked(val question: String,
-                            val choices: List<ChoiceUiState>) :
-        GameUiState {
-
-    }
+    data class AnswerChecked(
+        private val question: String,
+        private val choices: List<ChoiceUiState>
+    ) :
+        Abstract(
+            question,
+            choices,
+            checkVisibility = View.INVISIBLE,
+            nextVisibility = View.VISIBLE
+        )
 }
-interface ChoiceUiState{
 
-  data  class NotAvailableToChoose(val text: String) : ChoiceUiState {
-
-    }
-
-  data  class AvailableToChoose(val text: String) : ChoiceUiState {
-
-    }
-
-   data class Correct(val text: String) : ChoiceUiState {
-
-    }
-
-   data class Incorrect(val text: String) : ChoiceUiState {
-
-    }
-
-}
