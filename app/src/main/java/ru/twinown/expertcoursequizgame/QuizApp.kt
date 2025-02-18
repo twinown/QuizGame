8160e7a51884beb0f3ebce816452928cb3af07d5
twinown <niyaz9797@gmail.com>
2025-02-19 00:14:33 +0300

    QG-08 shared pref

--- app/src/main/java/ru/twinown/expertcoursequizgame/QuizApp.kt
+++ app/src/main/java/ru/twinown/expertcoursequizgame/QuizApp.kt
@@ -1,14 +1,22 @@
 package ru.twinown.expertcoursequizgame
 
 import android.app.Application
+import android.content.Context
 
 class QuizApp:Application() {
 
+    //создание шердов ты начал с инткэша. во время создания, но при запуске проги, естсессна,запускается
+    //сначала здесь
     //это и называется мануал диай
     lateinit var   viewModel: GameViewModel
 
     override fun onCreate() {
         super.onCreate()
-        viewModel = GameViewModel(GameRepository.Base())
+        //device explorer,чтоб найти этот файл/data/data/quizgane/sharedpref
+        val sharedPreferences = getSharedPreferences("QuizAppData",Context.MODE_PRIVATE)
+        viewModel = GameViewModel(GameRepository.Base(
+            IntCache.Base(sharedPreferences,"indexKey",0),
+            IntCache.Base(sharedPreferences,"userChoiceIndexKey",-1)
+        ))
     }
 }
